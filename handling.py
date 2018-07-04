cbd95758b9b75a32b736a608d4da32128050aad3
ksujit897 <ksujit898@gmail.com>
2018-07-04 10:54:11 +0530

    Error handling basic code

---
+++ handling.py
@@ -0,0 +1,23 @@
+try:
+
+    f = open("abc.txt")
+
+    p = 44445
+    b = 76476
+
+    p = b / 0
+
+
+
+
+    for line in f:
+        print(line)
+
+except FileNotFoundError as e:
+    print(e.filename)
+
+except Exception as e:
+    print(e)
+
+
+#i = 0/0
